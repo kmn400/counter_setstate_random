d1be7658cfc717910fa57a68b9315b99c88ee6e0
minho <kmn400@nate.com>
2022-02-14 16:29:50 +0900

    counter_setstate_random

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter = Random().nextInt(6);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              '$_counter',
+              style: TextStyle(fontSize: 205),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            RaisedButton(
+              onPressed: _incrementCounter,
+              child: const Text('random', style: TextStyle(fontSize: 30)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
